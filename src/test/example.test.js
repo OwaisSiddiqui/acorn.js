884b90335a9c14188a67476f831dae7381cad57c
OwaisSiddiqui <owais.siddiqui178@gmail.com>
2020-10-04 02:00:06 -0400

    Update TS files and Travis CI mocha test command

--- src/test/example.test.js
+++ src/test/example.test.js
@@ -1,9 +1,10 @@
-const assert = require('assert');
-describe('Simple Math Test', () => {
- it('should return 2', () => {
+"use strict";
+var assert = require('assert');
+describe('Simple Math Test', function () {
+    it('should return 2', function () {
         assert.equal(1 + 1, 2);
     });
- it('should return 9', () => {
+    it('should return 9', function () {
         assert.equal(3 * 3, 9);
     });
 });
